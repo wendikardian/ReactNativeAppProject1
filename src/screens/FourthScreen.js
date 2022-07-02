04e76fe760b489ee44181474e85e2f92691bd082
wendikardian <wendikardian@gmail.com>
2022-07-02 10:07:55 +0700

    ;/

--- src/screens/FourthScreen.js
+++ src/screens/FourthScreen.js
@@ -26,9 +26,14 @@ const FourthScreen = () => {
 const Challange4 = () => {
     return(
         <View style={{flex: 1}}>
-            <ImageBackground style={{flex: 1, justifyContent: 'center'}} imageStyle={{opacity: 0.5}} source={{uri:'https://i.pinimg.com/736x/e4/28/c5/e428c5f6e045bcf567fa4267f7985076.jpg'}}>
+            <ImageBackground style={{flex: 1, justifyContent: 'center'}} imageStyle={{opacity: 0.3}} source={{uri:'https://i.pinimg.com/736x/e4/28/c5/e428c5f6e045bcf567fa4267f7985076.jpg'}}>
                 <View style={{alignItems: 'flex-end'}}>
                 <Text style={{backgroundColor: '#F7F6B2', width: 100, padding: 10, borderRadius: 20, textAlign: 'center'}}>Colloseum</Text>
+                </View>
+                <View style={{margin: 8, padding: 8}}>
+                    <InputComponent title="Description" height={100} placeholder="About the place" multiline={true} keyboardType="default" />
+                    <InputComponent title="Phone Number" height={40} placeholder="Phone Number" multiline={true} keyboardType="numeric" />
+                    <InputComponent title="Location" height={40} placeholder="Location" multiline={true} keyboardType="default" />
                 </View>
             </ImageBackground>
         </View>
