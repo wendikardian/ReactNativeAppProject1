3e2912c0c67c82439de81c4d19fdbd94def4ec0c
wendikardian <wendikardian@gmail.com>
2022-07-02 11:27:31 +0700

    ;/

--- src/screens/InstagramHomeScreen.js
+++ src/screens/InstagramHomeScreen.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import {View, Text} from 'react-native'
-import {Icon} from 'react-native-elements'
-import {useFonts} from 'expo-font'
+import { Icon } from 'react-native-elements';
+// import {useFonts} from 'expo-font'
 
 const InstagramHomeScreen = () => {
-    const [loaded] = useFonts({
-        Montserrat: require('../../assets/fonts/StyleScript-Regular.ttf'),
-      });
+    // const [loaded] = useFonts({
+    //     Montserrat: require('../../assets/fonts/StyleScript-Regular.ttf'),
+    //   });
     return(
         <View style={{flex: 1}}>
             <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 <Text style={{fontFamily: 'StyleScript-Regular', fontSize: 40, marginLeft: 16}}>Instagram</Text>
-            </View>
-            <View style={{flexDirection : 'row', alignItems: 'center', marginRight: 8}}>
                 <View style={{margin: 8}}>
                     <Icon name='plus-square-o' type='font-awesome' size={26} />
                 </View>
