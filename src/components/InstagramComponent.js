f81649cc29b896f0e107d47677a3f5df8039da5e
wendikardian <wendikardian@gmail.com>
2022-07-11 15:29:26 +0700

    last meet 5

---
+++ src/components/InstagramComponent.js
@@ -0,0 +1,47 @@
+
+import React from "react"
+import {View, Text, Image} from "react-native"
+import {Icon} from "react-native-elements"
+
+export const Feed = (props) => {
+    const { username, post} = props;
+    return(
+        <View style={{marginBottom: 18}}>
+        <View style={{
+            flexDirection: 'row', 
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            margin: 8
+        }}>
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}>
+                <Image style={{width: 40, height: 40, borderRadius: 100}} source={props.imgUri} />
+                <Text style={{marginLeft: 8, fontWeight: 'bold'}}>{username}</Text>
+            </View>
+            <Icon name='dots-vertical' type='material-community' size={26} />
+        </View>
+        <Image source={props.post} style={{width: '100%', height: 300}} />
+        <View style={{
+            flexDirection: 'row', justifyContent: 'space-between'
+        }}
+        >
+            <View style={{flexDirection: 'row'}}>
+                <View style={{margin: 8}}>
+                    <Icon name='heart-o' type='font-awesome' size={26} />
+                </View>
+                <View style={{margin: 8}}>
+                    <Icon name='comment-o' type='font-awesome' size={26} />
+                </View>
+                <View style={{margin: 8}}>
+                    <Icon name='paper-plane-outline' type='ionicon' size={26} />
+                </View>
+            </View>
+            <View style={{margin: 8}}>
+                    <Icon name='bookmark-o' type='font-awesome' size={26} />
+            </View>
+        </View>
+        </View>
+    )
+}
--- src/screens/InstagramHomeScreen.js
+++ src/screens/InstagramHomeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, ScrollView} from 'react-native'
 // import { Icon } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome'
+import {Feed} from '../components/InstagramComponent.js'
 // import {useFonts} from 'expo-font'
 
 const InstagramHomeScreen = () => {
@@ -13,9 +14,17 @@ const InstagramHomeScreen = () => {
             <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 <Text style={{fontFamily: 'StyleScript-Regular', fontSize: 40, marginLeft: 16}}>Instagram</Text>
                 <View style={{margin: 8}}>
-                    <Icon name='rocket' type='font-awesome' size={26} />
+                    <Icon name='plus-square-o' type='font-awesome' size={26} />
                 </View>
             </View>
+            <ScrollView >
+                <Feed username="Wendi" imgUri={require('../../assets/images/profilePicture1.png')} post={{uri:'https://wallpaperaccess.com/full/201215.jpg'}}/>
+                <Feed username="Taylucifer" imgUri={require('../../assets/images/profilePicture2.png')} post={{uri:'https://images4.alphacoders.com/292/292026.jpg'}}/>
+                <Feed username="Blondie" imgUri={require('../../assets/images/profilePicture3.png')} post={{uri:'https://wallpaperaccess.com/full/496881.jpg'}}/>
+                <Feed username="AFadGuy" imgUri={require('../../assets/images/profilePicture4.png')} post={{uri:'https://images5.alphacoders.com/316/316297.jpg'}}/>
+                <Feed username="Fu9l1S1ut" imgUri={require('../../assets/images/profilePicture4.png')} post={{uri:'https://wallpaperaccess.com/full/1403923.jpg'}}/>
+                
+            </ScrollView>
         </View>
     )
 }
